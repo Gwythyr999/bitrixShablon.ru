bc474647e841e5ce97bfe44d102edb33109ae2be
Gwythyr999 <filipenko@ctweb.ru>
2017-07-06 12:47:03 +0700

    added inner shablon

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001499318513';
-$dateexpire = '001499322173';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1499318573;}';
+$datecreate = '001499319970';
+$dateexpire = '001499323630';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1499319991;}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/user_option/b5/b5fef86cde416d168f79a4d2cbdd0e09.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001499316338';
-$dateexpire = '001499319938';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:17:"helper_hero_admin";a:1:{s:4:"time";s:10:"1499316325";}}}';
-return true;
-?>
